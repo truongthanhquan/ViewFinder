44cf26cfbf459be432c61facb3c5c1c9ef4f1b2b
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-07-13 17:01:43 +0000

    "Upload"
    Former-commit-id: 6a5cb393893fdda2771616422f2968a0fa106f1f

--- upload.sh
+++ upload.sh
@@ -4,7 +4,7 @@ description=$1
 latest_tag=$(git describe --abbrev=0)
 grel release -u dosyago -r BrowserGap --tag $latest_tag --name "New release" --description '"'"$description"'"'
 grel upload -u dosyago -r BrowserGap --tag $latest_tag --name "BrowserGap.exe" --file BrowserGap.exe
-grel upload -u dosyago -r BrowserGap --tag $latest_tag --name "BrowserGap.macos" --file BrowserGap.mac
+grel upload -u dosyago -r BrowserGap --tag $latest_tag --name "BrowserGap.macos" --file BrowserGap.macos
 grel upload -u dosyago -r BrowserGap --tag $latest_tag --name "BrowserGap.linux" --file BrowserGap.nix
 grel upload -u dosyago -r BrowserGap --tag $latest_tag --name "BrowserGap.linx32" --file BrowserGap.nix32
 grel upload -u dosyago -r BrowserGap --tag $latest_tag --name "BrowserGap.win32.exe" --file BrowserGap.win32.exe

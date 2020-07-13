6f7c42119175b0b28b2130abbad602da0dc7312b
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-07-13 12:52:53 +0000

    "Updated sharp version. New errros."
    Former-commit-id: 01f628231a3bb30f3c1fd7a2768b7536cc1c7b6a

--- .nectar/coiny/lib/native_object.h
+++
@@ -1,5 +0,0 @@
-
-
-__NJS_DECL_FUNCTION<__NJS_VAR (vector<var>)>* __NJS_FN___q4iwau = new __NJS_DECL_FUNCTION<__NJS_VAR (vector<var>)>([]( vector<var> __NJS_VARARGS ) -> __NJS_VAR{var _obj; if(__NJS_VARARGS.size() > 0) _obj = __NJS_VARARGS[0];
-	return __NJS_Object_Keys(_obj);
-;return __NJS_Create_Undefined();});var __NJS_NATIVE_OBJECT_KEYS=__NJS_VAR(__NJS_FUNCTION, __NJS_FN___q4iwau);;

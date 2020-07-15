2fdb496d29a248773c2d23e9756fa7be742b6f41
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-07-15 07:24:56 +0000

    "OK"

--- new_release.sh
+++ new_release.sh
@@ -17,6 +17,7 @@ chmod +x BrowserGap.js
 gpush minor "New release"
 
 latest_tag=$(git describe --abbrev=0)
+
 grel release -u dosyago -r BrowserGap --tag $latest_tag --name "New release" --description '"'"$description"'"'
 grel upload -u dosyago -r BrowserGap --tag $latest_tag --name "BrowserGap.exe" --file BrowserGap.exe
 grel upload -u dosyago -r BrowserGap --tag $latest_tag --name "BrowserGap.macos" --file BrowserGap.macos
